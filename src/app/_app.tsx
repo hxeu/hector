9654438135ffd6fcb9a9524c23b3e3f7086263f9
hector <hx.euwx@gmail.com>
2024-01-22 17:49:26 -0500

    est ce que le css marche?6

---
+++ src/app/_app.tsx
@@ -0,0 +1,10 @@
+// src/app/_app.tsx
+
+import '../src/app/globals.css';
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
