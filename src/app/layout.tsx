44e20f4d20af921d652a95df05b2d630284bc772
hector <hx.euwx@gmail.com>
2024-01-28 17:45:47 -0500

    ok la vraiment vraiment promis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="../../public/favicon.ico" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
