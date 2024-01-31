67b0c6e4728884d45f31345987bfdfec1ec53f25
hector <hx.euwx@gmail.com>
2024-01-31 14:17:34 -0500

    petit changement dans hctr.tsx

--- pages/projects/hctr.tsx
+++ pages/projects/hctr.tsx
@@ -48,7 +48,7 @@ const HctrPage = () => {
         <div className=" mt-8 container mx-auto p-8 bg-white rounded-lg shadow-lg border-2 border-green-300/50">
           <h1 className="text-4xl font-bold mb-4 text-black">Creation of this Website</h1>
           <p className="text-gray-600">
-            Welcome to this website! I created this website to showcase my projects and skills. As you can see, this website is very simple. I wanted to keep it simple and clean, so I decided to use a minimalist design. I made it without using any template or low-code/no-code tools. I hope you enjoy your visit!
+            Welcome to this website! I created this website to showcase my projects and skills. I wanted to keep it simple and clean, so I decided to use a minimalist design. I made it without using any template or low-code/no-code tools. I hope you enjoy your visit!
           </p>
         </div>
 
