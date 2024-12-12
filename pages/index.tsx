a8706720f26fdbb1d53be34eb99c54c885fcfb76
hector <hx.euwx@gmail.com>
2024-12-12 21:37:21 +0100

    trying a new font

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ export default function Page() {
         <br />
         <br />
         <div className="relative z-10 text-center p-8">
-          <h1 className="text-black text-8xl mb-2 elastix">Hector<p className='protest'> </p>Menetrier</h1>
+          <h1 className="text-black text-8xl mb-2 protest">Hector Menetrier</h1>
           <p className="text-gray-500 mt-10">CS Student in France 🇫🇷, Denmark 🇩🇰 and Canada 🇨🇦 ! ( soon in Sweden 🇸🇪 )</p>
         </div>
 
