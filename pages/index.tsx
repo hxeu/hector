b6c26d9cc93532684a526810bbc33f7a7dde1d77
hector <hx.euwx@gmail.com>
2024-12-16 18:31:50 +0100

    responsive title in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 // index.tsx
-import { useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
-import Head from 'next/head';
-import '../src/app/globals.css';
-import ProjectsList from '../components/ProjectsList';
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import Head from "next/head";
+import "../src/app/globals.css";
+import ProjectsList from "../components/ProjectsList";
 
 export default function Page() {
   const [ref, inView] = useInView({
@@ -19,10 +19,32 @@ export default function Page() {
   }, [inView]);
 
   const projects = [
-    { id: 'hctr', title: 'My Website (hctr.store) 💻', description: 'How did I make this? 👨‍💻', date: '01/2024'},
-    { id: 'mauvais_soir', title: '"Mauvais Soir" 🌓', description: 'My monthly video series about my life! 🎥', date: '01/2022 - Today' },
-    { id: 'real_estate_swipe', title: 'Danish Real Estate App 🏢', description: 'Learn more about an app I did in Denmark for a school project. 🏠📱', date: '06/2023' },
-    { id: 'globetu', title: "Glob'Étu", description: 'Another app for a school project, this time in Canada. 🎓✈️📱', date: '12/2023'}
+    {
+      id: "hctr",
+      title: "My Website (hctr.store) 💻",
+      description: "How did I make this? 👨‍💻",
+      date: "01/2024",
+    },
+    {
+      id: "mauvais_soir",
+      title: '"Mauvais Soir" 🌓',
+      description: "My monthly video series about my life! 🎥",
+      date: "01/2022 - Today",
+    },
+    {
+      id: "real_estate_swipe",
+      title: "Danish Real Estate App 🏢",
+      description:
+        "Learn more about an app I did in Denmark for a school project. 🏠📱",
+      date: "06/2023",
+    },
+    {
+      id: "globetu",
+      title: "Glob'Étu",
+      description:
+        "Another app for a school project, this time in Canada. 🎓✈️📱",
+      date: "12/2023",
+    },
   ];
 
   return (
@@ -39,27 +61,42 @@ export default function Page() {
       <div
         ref={ref}
         className={`${
-          animateContent ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
+          animateContent
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 translate-y-10"
         } transition-opacity duration-1000 ease-in-out transform`}
       >
         <br />
         <br />
         <div className="relative z-10 text-center p-8">
-          <h1 className="text-black text-8xl mb-2 protest">Hector Menetrier</h1>
-          <p className="text-gray-600 mt-10">SSLIO Master Student in Sweden 🇸🇪 </p>
-          <p className="text-gray-600 ">Old CS Bachelor Student in France 🇫🇷, Denmark 🇩🇰 and Canada 🇨🇦 </p>
-          
+          <div className="relative z-10 text-center p-8 max-w-screen-md mx-auto">
+            <div className="relative z-10 text-center p-8 max-w-screen-md mx-auto flex flex-col justify-center items-center">
+              <h1 className="text-black text-6xl md:text-8xl mb-2 protest">
+                Hector Menetrier
+              </h1>
+            </div>
+          </div>
+
+          <p className="text-gray-600 mt-10">
+            SSLIO Master Student in Sweden 🇸🇪{" "}
+          </p>
+          <p className="text-gray-600 ">
+            Old CS Bachelor Student in France 🇫🇷, Denmark 🇩🇰 and Canada 🇨🇦{" "}
+          </p>
         </div>
 
         <div className="text-center p-4 text-gray-500">
-          Double Computer Science Graduate 🇫🇷 - 🇨🇦 - URCA-UQAC - Graduated in August 2024
+          Double Computer Science Graduate 🇫🇷 - 🇨🇦 - URCA-UQAC - Graduated in
+          August 2024
         </div>
 
         {/* Section des projets */}
         <div className="mt-8 mx-5">
-          <h2 className="text-3xl font-semibold mb-4 text-center font-mono text-black">My work :</h2>
+          <h2 className="text-3xl font-semibold mb-4 text-center font-mono text-black">
+            My work :
+          </h2>
           <ProjectsList projects={projects} />
-          <br/>
+          <br />
         </div>
       </div>
     </div>
